0ba93a551065b4b491a88676dad08b38713efa8a
Wouter Vernaillen <wouter@vernaillen.com>
2022-12-17 16:53:17 +0100

    fix e2e test

--- cypress/e2e/socialicons.cy.ts
+++ cypress/e2e/socialicons.cy.ts
@@ -1,8 +1,8 @@
 describe('Social Icons', () => {
   it('Contains 5 Icons', () => {
     cy.visit('/')
-    cy.get('#__nuxt > header > .wrapper > .socialIcons').should('have.length', 1)
-    cy.get('#__nuxt > header > .wrapper > .socialIcons > a').should('have.length', 5)
-    cy.get('#__nuxt > header > .wrapper > .socialIcons > a > svg').should('have.length', 5)
+    cy.get('#__nuxt > .main > .socialIcons').should('have.length', 1)
+    cy.get('#__nuxt > .main > .socialIcons > a').should('have.length', 5)
+    cy.get('#__nuxt > .main > .socialIcons > a > svg').should('have.length', 5)
   })
 })
