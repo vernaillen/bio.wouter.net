d3165dd33644a3f7895c71e340ec9787e9a3b01f
Wouter Vernaillen <wouter@vernaillen.com>
2023-02-04 19:37:01 +0100

    link to new blog post about Nuxt 3

--- composables/useLinks.ts
+++ composables/useLinks.ts
@@ -3,17 +3,11 @@ import type { Link } from '~~/types/link'
 export const useLinks = (): Link[] => {
   return [
     {
-      title: 'Trance Dance January 23',
-      subTitle: 'On January 23 we start with the first of a series of monthly Trance Dance sessions at vzw Dwarsligger in Melle',
-      url: 'https://harmonics.be/en/news/trancedance23january2023',
-      image: '/trancedance-fbevent-cover2.jpeg',
+      title: 'Nuxt 3',
+      subTitle: 'Latest blog post on vernaillen.dev about learning Nuxt 3',
+      url: 'https://vernaillen.dev/blog/nuxt',
+      image: '/nuxt-logo-green-light.png',
       expand: true,
-    },
-    {
-      title: 'Release Your Inner Dancer',
-      subTitle: 'Ecstatic Dance dj-set recorded on 3 September 2022 in Haaltert',
-      url: 'https://soundcloud.com/woutervernaillen/release-your-inner-dancer-ecstatic-dance',
-      soundcloud: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1337357878&color=%23ff5500&inverse=false&auto_play=false&show_user=true',
     },
     {
       title: 'Harmonics.be',
@@ -26,6 +20,12 @@ export const useLinks = (): Link[] => {
       subTitle: 'Senior Full Stack Developer',
       content: 'a brand new website for my freelance consultancy services as a full stack java developer',
       url: 'https://vernaillen.dev',
+    },
+    {
+      title: 'Release Your Inner Dancer',
+      subTitle: 'Ecstatic Dance dj-set recorded on 3 September 2022 in Haaltert',
+      url: 'https://soundcloud.com/woutervernaillen/release-your-inner-dancer-ecstatic-dance',
+      soundcloud: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1337357878&color=%23ff5500&inverse=false&auto_play=false&show_user=true',
     },
     {
       title: 'Vue AudioMotion Analyzer',
