78f3ed7678fd8191c0e6298a5f10f3cd04e483a4
Wouter Vernaillen <wouter@vernaillen.com>
2023-02-09 16:06:38 +0100

    Trance Dance 20 February + Nuxt 3.2.0

--- cypress/e2e/homepage.cy.ts
+++ cypress/e2e/homepage.cy.ts
@@ -5,18 +5,17 @@ describe('Homepage', () => {
     cy.contains('p', 'Wouter Vernaillen')
   })
 
-  it('Contains 6 links', () => {
+  it('Contains 7 links', () => {
     cy.visit('/')
-    cy.get('#__nuxt .itemWrapper').should('have.length', 6)
+    cy.get('#__nuxt .itemWrapper').should('have.length', 7)
 
     cy.get('#__nuxt .itemWrapper .item .details a').last().should('have.text', 'Vue AudioMotion Analyzer')
     cy.get('#__nuxt .itemWrapper .item .details .sub').last().should('have.text', 'High-resolution real-time audio spectrum analyzer')
   })
 
-  it('Contains expanded NUXT 3 link with img', () => {
+  it('First link is expanded', () => {
     cy.visit('/')
-    cy.get('#__nuxt .itemWrapper .item .details a').first().should('have.text', 'Trance Dance Music')
-    cy.get('#__nuxt .itemWrapper .item .details .sub').first().should('have.text', 'Listen to the music of our last trancedance session')
+    cy.get('#__nuxt .itemWrapper .item .details a').first().should('have.text', 'Trance Dance 20 February')
 
     cy.get('#__nuxt .itemExpandedWrapper').should('have.length', 1)
     cy.get('#__nuxt .itemExpandedWrapper').first().should('be.visible')
--- cypress/e2e/socialicons.cy.ts
+++ cypress/e2e/socialicons.cy.ts
@@ -1,5 +1,5 @@
 describe('Social Icons', () => {
-  it('Contains 5 Icons', () => {
+  it('Contains 4 Icons', () => {
     cy.visit('/')
     cy.get('#__nuxt > .main > .socialIcons').should('have.length', 1)
     cy.get('#__nuxt > .main > .socialIcons > a').should('have.length', 4)
