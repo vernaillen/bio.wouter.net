06ef0d9cba7e7376d1f34df6a0a6107aa5ade0b2
Wouter Vernaillen <wouter@vernaillen.com>
2023-03-14 19:10:12 +0100

    github img width & height

--- components/GitHub.vue
+++ components/GitHub.vue
@@ -11,7 +11,7 @@ const { data: commits } = useLazyAsyncData<CommitResponse[]>('commits', () => $f
     <span v-if="commits && commits[0]"><br>
       <a :href="commits[0].html_url" target="_blank">last commit: {{ dayjs (commits[0].commit.committer.date).format("DD MMM YYYY") }}</a>
       by <a :href="commits[0].author.html_url" target="_blank">
-        <img v-if="commits[0].author.avatar_url" alt="Avatar of GitHub commit author" class="avatar-user inline-block" :src="commits[0].author.avatar_url">
+        <img v-if="commits[0].author.avatar_url" width="13" height="16" alt="Avatar of GitHub commit author" class="avatar-user inline-block" :src="commits[0].author.avatar_url">
         {{ commits[0].author.login }}
       </a>
     </span>
