f1ce5835d5b54a4a2605195f13b7bd52995adb1a
Wouter Vernaillen <wouter@vernaillen.com>
2023-10-07 09:46:12 +0200

    fix e2e test, always use English

--- cypress/e2e/homepage.cy.ts
+++ cypress/e2e/homepage.cy.ts
@@ -2,23 +2,23 @@ import { useLinks } from '../../composables/useLinks'
 
 describe('Homepage', () => {
   it('Contains intro', () => {
-    cy.visit('/')
+    cy.visit('/en')
     cy.contains('h3', 'wouter on the net')
     cy.contains('div', 'Wouter Vernaillen')
   })
 
   it('Contains correct number of links', () => {
-    cy.visit('/')
-    cy.get('div#theLinks > div > div > button').should('have.length', useLinks().length)
+    cy.visit('/en')
+    cy.get('div#theLinks > div > div > button').should('have.length', useLinks('en').length)
   })
 
   it('First & last link contain correct label', () => {
-    cy.visit('/')
+    cy.visit('/en')
 
-    const firstLink = useLinks()[0]
+    const firstLink = useLinks('en')[0]
     cy.get('div#theLinks > div > div > button').first().should('contain.text', firstLink.label)
 
-    const lastLink = useLinks()[useLinks().length - 1]
+    const lastLink = useLinks('en')[useLinks('en').length - 1]
     cy.get('div#theLinks > div > div > button').last().should('contain.text', lastLink.label)
   })
 })
