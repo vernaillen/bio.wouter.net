ec8012e95ee3af2cdd8ab57c7d5624b4c1d90e83
Wouter Vernaillen <wouter@vernaillen.com>
2024-03-13 07:39:27 +0100

    fix cypress homepage tests

--- cypress/e2e/homepage.cy.ts
+++ cypress/e2e/homepage.cy.ts
@@ -9,16 +9,16 @@ describe('Homepage', () => {
 
   it('Contains correct number of links', () => {
     cy.visit('/en')
-    cy.get('div#theLinks > div > div > button').should('have.length', useLinks('en').length)
+    cy.get('div#theLinks > div > div > div > button').should('have.length', useLinks('en').length)
   })
 
   it('First & last link contain correct label', () => {
     cy.visit('/en')
 
     const firstLink = useLinks('en')[0]
-    cy.get('div#theLinks > div > div > button').first().should('contain.text', firstLink.label)
+    cy.get('div#theLinks > div > div > div > button').first().should('contain.text', firstLink.label)
 
     const lastLink = useLinks('en')[useLinks('en').length - 1]
-    cy.get('div#theLinks > div > div > button').last().should('contain.text', lastLink.label)
+    cy.get('div#theLinks > div > div > div > button').last().should('contain.text', lastLink.label)
   })
 })
